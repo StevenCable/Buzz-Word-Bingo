ecc91ce5fd3d21672e9ebcece5b1cc5c7ac0aeaa
Steven Cable <steven.cable12@gmail.com>
2017-01-04 20:56:06 -1000

    created json and index.js files

---
+++ index.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+const express = require('express');
+
+var buzz = express();
+
+app.get();
+
+
+
+
+var server = app.listen(3000, () =>{
+  var host = server.address().address;
+  var port = server.address().port;
+
+
+  console.log('BuzzWord App listening at http://%s:%s', host, port);
+});
